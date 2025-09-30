3d65f16d8fe783eebd02abde9e000f04f240a931
MinimalMikir <bananaskiee@gmail.com>
2025-10-01 03:11:09 +0700

    Create prensenceUpdate.js

---
+++ events/prensenceUpdate.js
@@ -0,0 +1,10 @@
+const updateOnline = require("../online");
+
+module.exports = {
+  name: "presenceUpdate",
+  async execute(_, newPresence, client) {
+    const guild = newPresence.guild;
+    if (!guild) return;
+    await updateOnline(guild);
+  },
+};
