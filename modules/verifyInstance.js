e2bc3245cd5f815f3930e9f3b9c503f89e2fc931
MinimalMikir <bananaskiee@gmail.com>
2025-10-15 19:05:28 +0700

    Delete modules/verifyInstance.js

--- modules/verifyInstance.js
+++
@@ -1,8 +0,0 @@
-const VerifySystem = require('./verify');
-
-// ✅ BUAT SINGLE INSTANCE DI SINI SAJA
-const verifySystem = new VerifySystem();
-
-console.log('✅ VerifySystem single instance created');
-
-module.exports = verifySystem;
