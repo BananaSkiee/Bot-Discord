0c40f604f4ea3685a028b7316845d8e02a6afdb6
MinimalMikir <bananaskiee@gmail.com>
2025-11-26 23:46:45 +0700

    Remove countValidator import from messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, AttachmentBuilder, EmbedBuilder } = require("discord.js");
 
-const countValidator = require("../modules/countValidator");
 const handleHapusTag = require("../modules/hapusTagCommand");
 const translateHandler = require("../modules/translate");
 const memeCommand = require("../modules/memeCommand");
